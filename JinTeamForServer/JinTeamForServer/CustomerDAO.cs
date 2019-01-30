124d8af0212b63de39fc3894a7c3daf58b05a11f
ku718 <ku718@naver.com>
2019-01-30 18:18:35 +0900

    웹에서 자마린으로 데이터 보내기 확인 / - 응답코드 만드는중

--- JinTeamForServer/JinTeamForServer/CustomerDAO.cs
+++ JinTeamForServer/JinTeamForServer/CustomerDAO.cs
@@ -17,7 +17,7 @@ namespace JinTeamForServer
             SqlParameter[] sqlp = { new SqlParameter("sellerId", sellerID) };
             try
             {
-                result = con.SendScalarReadQuery(query, sqlp);
+                result = con.ChkData(query, sqlp);
             }
             catch (Exception)
             {
@@ -27,7 +27,7 @@ namespace JinTeamForServer
         }
 
         public void InsertCustomer(Customer cus)
-        {            
+        {
             string query = "insert_cus";
             SqlParameter[] sqlp = { new SqlParameter("cus_ID", cus.Cus_ID), new SqlParameter("cus_pwd", cus.Cus_pwd), new SqlParameter("Cus_phone", cus.Cus_phone), new SqlParameter("Cus_addr", cus.Cus_addr), new SqlParameter("Cus_name", cus.Cus_name), new SqlParameter("Cus_Nickname", cus.Cus_Nickname), new SqlParameter("Cus_gender", cus.Cus_gender), new SqlParameter("Cus_age", cus.Cus_age) };
             try
@@ -38,6 +38,39 @@ namespace JinTeamForServer
             {
                 throw;
             }
+        }
+
+        public bool chkID(string cus_ID)
+        {
+            bool result = false;
+            string query = "chkID";
+            SqlParameter[] sqlp = { new SqlParameter("cus_ID", cus_ID) };
+            try
+            {
+                result = con.ChkData(query, sqlp);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return result;
+        }
+
+
+        public bool ChkPhone(string cus_phone)
+        {
+            bool result = false;
+            string query = "chkPhone";
+            SqlParameter[] sqlp = { new SqlParameter("cus_phone", cus_phone) };
+            try
+            {
+                result = con.ChkData(query, sqlp);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return result;
         }
     }
 }
--- JinTeamForServer/JinTeamForServer/FrmInsertCust.aspx.cs
+++ JinTeamForServer/JinTeamForServer/FrmInsertCust.aspx.cs
@@ -9,6 +9,8 @@ namespace JinTeamForServer
 {
     public partial class FrmInsertCust : System.Web.UI.Page
     {
+        string errCode;
+
         string cus_ID;
         string cus_pwd;
         string cus_phone;
@@ -20,12 +22,40 @@ namespace JinTeamForServer
         CustomerDAO cust = new CustomerDAO();
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Request.Params["cus_ID"] != null && Request.Params["cus_pwd"] == null)
+            {
+                cus_ID = Request.Params["cus_ID"];
+                if (cust.chkID(cus_ID))
+                {
+                    errCode = "202";    // ID 중복 에러
+                    Response.Write(errCode);
+                    Response.Flush();
+                    return;
+                }
+                else
+                {
+                    // 이메일 보내기
+                    return;
+                }
+            }
+
+            if (Request.Params["cus_phone"] != null)
+            {
+                cus_phone = Request.Params["cus_phone"];
+                if (cust.ChkPhone(cus_phone))
+                {
+                    errCode = "201";    // phone 유니크 에러
+                    Response.Write(errCode);
+                    Response.Flush();
+                    return;
+                }
+            }else
+            
             //cus_ID, pwdencrypt(cus_pwd), cus_phone, cus_addr, cus_name, cus_Nickname, cus_gender, cus_age
-            if (Request.Params["cus_ID"] != null && Request.Params["cus_pwd"] != null && Request.Params["cus_phone"] != null && Request.Params["cus_addr"] != null && Request.Params["cus_name"] != null && Request.Params["cus_Nickname"] != null && Request.Params["cus_gender"] != null && Request.Params["cus_age"] != null)
+            if (Request.Params["cus_ID"] != null && Request.Params["cus_pwd"] != null && Request.Params["cus_addr"] != null && Request.Params["cus_name"] != null && Request.Params["cus_Nickname"] != null && Request.Params["cus_gender"] != null && Request.Params["cus_age"] != null)
             {
                 cus_ID = Request.Params["cus_ID"];
                 cus_pwd = Request.Params["cus_pwd"];
-                cus_phone = Request.Params["cus_phone"];
                 cus_addr = Request.Params["cus_addr"];
                 cus_name = Request.Params["cus_name"];
                 cus_Nickname = Request.Params["cus_Nickname"];
@@ -34,10 +64,19 @@ namespace JinTeamForServer
             }
             else
             {
-                
+                errCode = "300";    // 값 입력이 되지않음
             }
             //Customer cust = new Customer();
-            cust.InsertCustomer(new Customer(cus_ID, cus_pwd, cus_phone, cus_addr, cus_name, cus_Nickname, cus_gender, cus_age));
+            try
+            {
+                cust.InsertCustomer(new Customer(cus_ID, cus_pwd, cus_phone, cus_addr, cus_name, cus_Nickname, cus_gender, cus_age));
+                errCode = "100";    // 회원가입 성공
+            }
+            catch (Exception)
+            {
+                errCode = "200";    // 실패
+            }
+            Response.Write(errCode);
         }
     }
 }
